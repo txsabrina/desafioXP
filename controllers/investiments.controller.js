0f49518e995092e19b5ea450236270abc76d143b
Sabrina <stx.sabrina@gmail.com>
2022-07-23 16:00:18 -0300

    feat(controller): adiciona arquivo e função de investimento em ativos

---
+++ controllers/investiments.controller.js
@@ -0,0 +1,17 @@
+const service = require('../services/investments.service');
+const buyAsserts = async (req, res, next) => {
+  const { codAsset, qttAssets } = req.body;
+  const { codClient } = req.client;
+  
+  try{
+    await service.buyAsserts(codClient, codAsset, qttAssets);
+    return res.status(200).json({ message: 'Successful purchase!'});
+    
+  } catch(e) {
+    return next(e);
+  };
+};
+
+module.exports = {
+  buyAsserts,
+};
