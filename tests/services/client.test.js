2e8f1ae351a3b522e2f7984399417f2eed23f465
Sabrina <stx.sabrina@gmail.com>
2022-07-23 19:37:20 -0300

    test(service): implementa os testes pra função de login

--- tests/services/client.test.js
+++ tests/services/client.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const { Client } = require('../../src/database/models');
-const { createClient } = require('../../src/services/client.service');
+const { createClient, login } = require('../../src/services/client.service');
 
 describe('Quando chamada a função create', () => {
   describe('caso ja exista um usuario com o email', () => {
@@ -64,3 +64,43 @@ describe('Quando chamada a função create', () => {
     });
   }); 
 })
+
+describe('Quando chamada a função login', () => {
+  describe('em caso', () => {
+    before(sinon.restore);
+
+    const email = 'sabrin@gmail.com';
+    const pass = '1232039483'
+    
+    sinon.stub(Client, 'findOne').throws;
+    
+    it('o email seja invalido, deve retornar um erro com a mensagem "Invalid email!" ', async () => {
+      try {
+        await login(email, pass);
+      } catch (e) {
+        expect(e.message).to.be.equal('Invalid email!')
+      }
+    });
+  });
+  
+  describe('em caso de sucesso', async () => {
+    const mockClient = {
+      codClient: 7,
+      name: 'sabrina',
+      email: 'sabrin@gmail.com',
+      password: '123455668',
+      balance: 0,
+    };
+
+    const email = 'sabrin@gmail.com';
+    const pass = '1232039483'
+    
+    sinon.stub(Client, 'findOne').resolves(mockClient);
+
+    const result = await login(email, pass);
+    
+    it('deve retornar um token', () => {
+      expect(result).to.be.a('string');
+    });
+  });
+});
