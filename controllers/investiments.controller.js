2be52278650814d3219ba44464c7ed43fabd7a2d
Sabrina <stx.sabrina@gmail.com>
2022-07-23 16:14:01 -0300

    feat(controller): adiciona função de comprar Ativos

--- controllers/investiments.controller.js
+++ controllers/investiments.controller.js
@@ -7,11 +7,24 @@ const buyAsserts = async (req, res, next) => {
     await service.buyAsserts(codClient, codAsset, qttAssets);
     return res.status(200).json({ message: 'Successful purchase!'});
     
-  } catch(e) {
-    return next(e);
+  } catch(err) {
+    return next(err);
+  };
+};
+
+const sellAsserts = async (req, res, next) => {
+  const { codAsset, qttAssets } = req.body;
+  const { codClient } = req.client;
+
+  try {
+    await service.sellAsserts(codClient, codAsset, qttAssets);
+    return res.status(200).json({ message: 'Successful sale!'});
+  } catch (err) {
+    return next(err)
   };
 };
 
 module.exports = {
   buyAsserts,
+  sellAsserts,
 };
