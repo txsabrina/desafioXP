4a853c88a4c7a26d7305198e964d01d833d5000e
Sabrina <stx.sabrina@gmail.com>
2022-07-24 13:18:55 -0300

    test(controller): implementa teste do controller da  função de criar clientes

---
+++ tests/controller/client.test.js
@@ -0,0 +1,40 @@
+const { createClient, login } = require('../../src/controllers/client.controller');
+const service = require('../../src/services/client.service');
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+describe('Ao chamar o createClient do controller', () => {
+  describe('quando o payload for validado', () => {  
+    const req = {};
+    const res = {};
+    const next = sinon.spy();
+    beforeEach(() => {
+      req.body = {
+        name: 'sabrina',
+        email: 'sabrin@gmail.com',
+        password: '1233445567'
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.send = sinon.stub().returns();
+
+      sinon.stub(service, 'createClient').resolves(true);
+    });
+
+    afterEach(() => {
+      service.createClient.restore();
+    })
+
+    it('retorna um o status 201', async () => {    
+
+      await createClient(req, res, next);
+      expect(res.status.calledWith(201)).to.be.true;
+    });
+    
+    it('o response é um objeto', async () => {
+      await createClient(req, res, next);
+
+      expect(res).to.be.an('object');
+    });
+  });
+});
